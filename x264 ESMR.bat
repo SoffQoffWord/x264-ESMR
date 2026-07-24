f0a88a8d98d435fb3d1dd3181f76e526effbd2df
SoffQoff Word <83302448+SoffQoffWord@users.noreply.github.com>
2026-07-24 22:18:56 +0200

    Update batch script for output directory and messaging

--- x264 ESMR.bat
+++ x264 ESMR.bat
@@ -1,14 +1,14 @@
 @echo off
-set "outdir=%~dp1_PROPRE"
+set "outdir=%~dp1_TASK"
 if not exist "%outdir%" mkdir "%outdir%"
 
 :loop
 if "%~1"=="" goto end
 
 echo.
-echo Traitement : %~nx1
+echo Processing : %~nx1
 
-ffmpeg -hide_banner -i "%~1" -map 0 -c copy -bsf:v "filter_units=remove_types=6" -map_metadata:g -1 -y "%outdir%\%~n1_propre.mkv"
+ffmpeg -hide_banner -i "%~1" -map 0 -c copy -bsf:v "filter_units=remove_types=6" -map_metadata:g -1 -y "%outdir%\%~n1_task.mkv"
 
 if errorlevel 1 (
     echo ERROR : %~nx1
@@ -21,5 +21,5 @@ goto loop
 
 :end
 echo.
-echo Termine.
+echo Finish.
 pause
